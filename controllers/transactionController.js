7c570a39990f68f28a2d5e290f3f672bd2974739
Mreena Kousar <mrina9035@gmail.com>
2025-11-15 23:26:37 -0800

    pagination

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -3,7 +3,7 @@ const Transaction = require("../models/Transaction");
 exports.getTransactions = async (req, res) => {
    try {
     // frontend se ?category=office wala value ayegi
-    const { category } = req.query;
+    const { category, page = 1, limit = 20 } = req.query;
     let filter = {};
 
 
@@ -11,9 +11,13 @@ exports.getTransactions = async (req, res) => {
       filter.category = { $regex: category, $options: "i" }; 
     }
  
-   
-    const transactions = await Transaction.find(filter).sort({ date: -1 });
-    res.status(200).json({ success: true, transactions });
+   const skip = (parseInt(page) - 1) * parseInt(limit);
+    const transactions = await Transaction.find(filter).sort({ date: -1 }).skip(skip).limit(parseInt(limit));
+    const totalCount = await Transaction.countDocuments(filter);
+    const totalPages = Math.ceil(totalCount / limit);
+
+
+    res.status(200).json({ success: true, transactions, page: parseInt(page),  totalPages,  totalCount, });
   } catch (error) {
     console.error(error);
     res.status(500).json({ success: false, msg: "Failed to fetch transactions" });
