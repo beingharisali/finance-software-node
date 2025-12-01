31cee3f2b34c2707ad9e23421f87d21af45b096c
Mreena Kousar <mrina9035@gmail.com>
2025-12-01 06:41:59 -0800

    task

--- models/Sale.js
+++ models/Sale.js
@@ -1,4 +1,42 @@
 
+// const mongoose = require("mongoose");
+
+// const SaleSchema = new mongoose.Schema(
+//   {
+//     productType: {
+//       type: String,
+//       enum: ["Gold", "Whisky"],
+//       required: true,
+//     },
+//     productId: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     productDescription: {
+//       type: String,
+//       required: true,
+//     },
+//     price: {
+//       type: Number,
+//       required: true,
+//       min: 0,
+//     },
+//     broker: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "User",
+//       required: true,
+//     },
+//     commission: {
+//       type: Number,
+//       required: true,
+//       min: 0,
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model("Sale", SaleSchema);
 const mongoose = require("mongoose");
 
 const SaleSchema = new mongoose.Schema(
@@ -23,18 +61,14 @@ const SaleSchema = new mongoose.Schema(
       min: 0,
     },
     broker: {
-      type: String,
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
       required: true,
     },
     commission: {
       type: Number,
       required: true,
       min: 0,
-    },
-    agent: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
     },
   },
   { timestamps: true }
