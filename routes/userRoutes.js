04519fccc6d81e9dccc2e17ce94797f0b015df24
Mreena Kousar <mrina9035@gmail.com>
2025-11-30 22:16:49 -0800

    newly

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,28 @@
 
+// // const express = require("express");
+// // const router = express.Router();
+// // const authenticateUser = require("../middleware/authentication");
+// // const allowRoles = require("../middleware/roleMiddleware");
+// // const {
+// //   createUser,
+// //   getUsers,
+// //   updateUser,
+// //   deleteUser,
+// // } = require("../controllers/userController");
+
+// // // Admin + Manager can create users
+// // router.post("/", authenticateUser, allowRoles("admin", "manager"), createUser);
+
+// // // Admin + Manager can fetch users
+// // router.get("/", authenticateUser, allowRoles("admin", "manager"), getUsers);
+
+// // // Admin can update users
+// // router.put("/:id", authenticateUser, allowRoles("admin"), updateUser);
+
+// // // Admin can delete users
+// // router.delete("/:id", authenticateUser, allowRoles("admin"), deleteUser);
+
+// // module.exports = router;
 // const express = require("express");
 // const router = express.Router();
 // const authenticateUser = require("../middleware/authentication");
@@ -10,16 +34,9 @@
 //   deleteUser,
 // } = require("../controllers/userController");
 
-// // Admin + Manager can create users
 // router.post("/", authenticateUser, allowRoles("admin", "manager"), createUser);
-
-// // Admin + Manager can fetch users
 // router.get("/", authenticateUser, allowRoles("admin", "manager"), getUsers);
-
-// // Admin can update users
 // router.put("/:id", authenticateUser, allowRoles("admin"), updateUser);
-
-// // Admin can delete users
 // router.delete("/:id", authenticateUser, allowRoles("admin"), deleteUser);
 
 // module.exports = router;
@@ -34,9 +51,17 @@ const {
   deleteUser,
 } = require("../controllers/userController");
 
+// Admin + Manager can create users
 router.post("/", authenticateUser, allowRoles("admin", "manager"), createUser);
-router.get("/", authenticateUser, allowRoles("admin", "manager"), getUsers);
+
+//  Admin + Manager + Assistant can fetch users  
+router.get("/", authenticateUser, allowRoles("admin", "manager", "assistant"), getUsers);
+
+// Admin can update users
 router.put("/:id", authenticateUser, allowRoles("admin"), updateUser);
+
+// Admin can delete users
 router.delete("/:id", authenticateUser, allowRoles("admin"), deleteUser);
 
 module.exports = router;
+// hello
